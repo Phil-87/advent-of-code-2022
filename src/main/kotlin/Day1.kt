44150f0dbbd3a9e4fc8ecac2caafdc6ba39d4b84
Phil Turner <philip.turner@skybettingandgaming.com>
2022-12-02 16:19:47 +0000

    Day 1 part 1 complete

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -5,5 +5,23 @@ fun day1() {
 
     val foods = File("src/main/kotlin/day1-input.txt").readLines()
 
-    val x = 0
+    val elves = mutableListOf<Int>()
+    var calories = 0
+
+    foods.forEach {
+        if (it.isNotEmpty())
+            calories += it.toInt()
+        else {
+            elves.add(calories)
+            calories = 0
+        }
+    }
+
+    // Add last one
+    if (calories > 0)
+        elves.add(calories)
+
+    val max = elves.maxOrNull() ?: 0
+
+    println("Max calories: $max")
 }
